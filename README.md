9068b2f7d9062aad085b3fce0fbf496f8292e6b0
Jillian Tang <jilingt@stanford.edu>
2019-10-27 13:54:53 -0700

    read in data

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # unifyid-keystroke
 Coding challenge for UnifyID
+
+1. Run `Rscript read_data.R` to make the raw csv from the JSON files.
---
+++ read_data.R
@@ -0,0 +1,21 @@
+library(jsonlite)
+library(data.table)
+
+MAX_USERS <- 1000 # assume max of 1000 users - this makes reading faster
+
+first <- "user_4a438fdede4e11e9b986acde48001122.json"
+alldat <- vector("list", MAX_USERS) 
+i <- 1
+while(!is.null(first)) {
+  print(paste("reading data from file", first))
+  dat <- fromJSON(paste0("https://challenges.unify.id/v1/mle/", first))
+  names(dat[[1]]) <- seq(1, length(dat[[1]]))
+  flatdat <- do.call(rbind, unname(Map(cbind, entry = seq(1, length(dat[[1]])), dat[[1]])))
+  flatdat$user <- dat[[2]]
+  alldat[[i]] <- flatdat
+  i <- i + 1
+  first <- dat[[3]]
+}
+alldat[sapply(alldat, is.null)] <- NULL
+alldat <- do.call(rbind, alldat)
+fwrite(alldat, "raw_allusers.csv")
